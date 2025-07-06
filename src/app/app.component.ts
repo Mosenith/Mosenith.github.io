816080372ce673cb8ecfe70404c1ee60a792bad2
Mosenith Heang <heang.mosenith.1206@gmail.com>
2025-07-06 17:36:48 -0500

    Update Title & Contact Description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,18 +20,18 @@ export class AppComponent implements OnInit {
     this.languageService.initLanguage();
 
     this.titleService.setTitle(
-      "Mosenith Heang | Dotnet Developer | Java Developer | Software Engineer"
+      "Mosenith Heang | Full Stack .NET Developer | Software Engineer"
     );
     this.metaService.addTags([
       {
         name: "keywords",
         content:
-          "C#, Dotnet Developer , Java Developer , Software Engineer, software, developer",
+          "C#, Full Stack .NET Developer , Software Engineer, Software Developer",
       },
       {
         name: "description",
         content:
-          "With over 4 years of experience as a Software Engineer, I have honed my expertise in developing distributed systems using C#, ASP.NET Core, Java, and Spring Boot. My portfolio showcases a diverse range of projects involving RESTful APIs, cloud platforms like AWS, DevOps tools such as Azure DevOps, and both SQL and NoSQL databases. Additionally, I have gained valuable experience working with Natural Language Processing and Machine Learning techniques.",
+          "With over 4 years of experience as a Full Stack .NET Developer, I have honed my expertise in developing distributed systems using C#, ASP.NET Core, Java, and Spring Boot. My portfolio showcases a diverse range of projects involving RESTful APIs, cloud platforms like AWS, DevOps tools such as Azure DevOps, and both SQL and NoSQL databases. Additionally, I have gained valuable experience working with Natural Language Processing and Machine Learning techniques.",
       },
     ]);
 
